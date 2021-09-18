dc2d9f551b9c707f5f69fdaf2b6a4e615da913ff
yimt <2579881587@qq.com>
2021-09-18 19:58:56 +0800

    Add java examples for two number

---
+++ algorithms/0001_two-sum/java/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Solution s = new Solution();
+        int[] result = s.twoSum(new int[]{2,7,11,15}, 9);
+        System.out.println(Arrays.toString(result));
+    }
+}
+
