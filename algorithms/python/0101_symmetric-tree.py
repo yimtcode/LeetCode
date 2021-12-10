372293e4b7b9230d3489aef58186579bc92b9d86
yimt <2579881587@qq.com>
2021-12-10 11:54:07 +0800

    Update symmetric tree in the Python

--- algorithms/python/0101_symmetric-tree.py
+++ algorithms/python/0101_symmetric-tree.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 # Definition for a binary tree node.
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
@@ -7,6 +10,7 @@ class TreeNode(object):
 
 
 class Solution(object):
+    # recursion
     def isSymmetric(self, root):
         return self.isMirror(root.left, root.right)
 
@@ -22,6 +26,27 @@ class Solution(object):
 
         return self.isMirror(a.left, b.right) and self.isMirror(a.right, b.left)
 
+    # iteration
+    def isSymmetric2(self, root):
+        q = deque([root.left, root.right])
+        while len(q) > 0:
+            a, b = q.popleft(), q.popleft()
+            if not a and not b:
+                continue
+
+            if not a or not b:
+                return False
+
+            if a.val != b.val:
+                return False
+
+            q.append(a.left)
+            q.append(b.right)
+            q.append(a.right)
+            q.append(b.left)
+
+        return True
+
 
 if __name__ == '__main__':
     root = TreeNode(1,
@@ -32,3 +57,5 @@ if __name__ == '__main__':
     s = Solution()
     b = s.isSymmetric(root)
     print(b)
+    b2 = s.isSymmetric(root)
+    print(b2)
